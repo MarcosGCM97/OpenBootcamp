eac9ae5fa3ec66fee80403ce0e2bd090cf6f07c2
MarcosGCM97 <109760848+MarcosGCM97@users.noreply.github.com>
2022-09-28 10:16:57 -0300

    Add files via upload

---
+++ actividad2.js
@@ -0,0 +1,11 @@
+let nombre = "Marcos Gabriel";
+let edad = 25;
+let eresDesarrollador = true;
+const fecha_nacimiento = new Date(2022, 5, 1997);
+let libroFavorito = {
+    titulo : "el nudo de la conciencia",
+    autor : "Enzo Tagliazucchi",
+    colabracion : "El Gato y la Caja",
+    fecha : new Date(23, 5, 2021),
+    url : "https://elgatoylacaja.com/elnudo/indice"
+}
